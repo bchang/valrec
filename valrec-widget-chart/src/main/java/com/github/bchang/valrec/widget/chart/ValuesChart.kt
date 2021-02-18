bcf97869f5f605a95456008f6edebd2260a78657
Brian Chang <bchanged@gmail.com>
2021-02-17 23:01:50 -0800

    Chart now uses datetime scale.
    Put in some preliminary intervals which work well for 24h. Tried setting date format for labels, but it's not working as expected (see comment).
    
    There's some issue with hot-applying code changes while codeing, something to do about id/valueschart. I think this is something about view state and can be addressed by making ValuesChart into a Fragment.

--- valrec-widget-chart/src/main/java/com/github/bchang/valrec/widget/chart/ValuesChart.kt
+++ valrec-widget-chart/src/main/java/com/github/bchang/valrec/widget/chart/ValuesChart.kt
@@ -8,7 +8,12 @@ import com.anychart.AnyChart
 import com.anychart.AnyChartView
 import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.anychart.data.Set
+import com.anychart.enums.Interval
+import com.anychart.scales.DateTime
 import com.github.bchang.valrec.datastore.Record
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
 
 class ValuesChart @JvmOverloads constructor(
     context: Context,
@@ -18,24 +23,49 @@ class ValuesChart @JvmOverloads constructor(
     Observer<List<Record>> {
 
     private val anyChartView = AnyChartView(context, attrs, defStyleAttr)
-    private val cartesian = AnyChart.line()
+    private val chart = AnyChart.line()
+    private val dateTimeScale = DateTime.instantiate()
 
     init {
+        chart.xScale(dateTimeScale)
+        anyChartView.setChart(chart)
         addView(anyChartView)
-
-        cartesian.animation(true)
-        cartesian.title("Values over time")
-        cartesian.yAxis(0, "Value")
-        cartesian.xAxis(0, "Date")
-        anyChartView.setChart(cartesian)
     }
 
     override fun onChanged(records: List<Record>) {
         val acSet = Set.instantiate()
+        chart.yScale().ticks().interval(1)
+        // Strangely this will render labels all as "08:00:00",
+        // definitely something to do with being in PST. It's labeling all
+        // ticks as if converted back to GMT, then dropped any time mod so
+        // that all labels are actually modeled by the same instant.
+//        chart.xAxis(0).labels().format(
+//            "{%value}{type:time}"
+//        )
+        // Works well for 24h - 8 ticks
+        dateTimeScale.ticks().interval(Interval.HOUR, 3)
         acSet.data(records.map {
-            ValueDataEntry(it.timestamp.toString(), it.value)
+            ValueDataEntry(
+                DATE_TIME_FORMATTER
+                    .withZone(ZoneId.systemDefault())
+                    .format(it.timestamp),
+                it.value
+            )
         })
 
-        cartesian.removeAllSeries().addSeries(acSet)
+        chart.removeAllSeries().addSeries(acSet)
+    }
+
+    companion object {
+        private val DATE_TIME_FORMATTER =
+            DateTimeFormatterBuilder()
+                .append(DateTimeFormatter.ISO_LOCAL_DATE)
+                .appendLiteral('T')
+                .append(DateTimeFormatter.ISO_LOCAL_TIME)
+                // Fool the JS engine into thinking we're dealing in GMT times.
+                // This seems to be the only solution unless I can find a way to
+                // give the JS engine a different time zone for date format output.
+                .appendLiteral('Z')
+                .toFormatter()
     }
 }
