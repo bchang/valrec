919e9f5abe782f121ea6b345ff196f3b9979c1e1
Brian Chang <bchanged@gmail.com>
2021-02-20 15:50:01 -0800

    Use a more readable datetime format in the values table.

--- valrec-widget-table/src/main/java/com/github/bchang/valrec/widget/table/ValuesTableAdapter.kt
+++ valrec-widget-table/src/main/java/com/github/bchang/valrec/widget/table/ValuesTableAdapter.kt
@@ -4,18 +4,24 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
-import com.github.bchang.valrec.widget.table.databinding.ValuesTableRowItemBinding
 import com.github.bchang.valrec.datastore.Record
+import com.github.bchang.valrec.widget.table.databinding.ValuesTableRowItemBinding
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class ValuesTableAdapter :
     RecyclerView.Adapter<ValuesTableAdapter.ViewHolder>(),
     Observer<List<Record>> {
-    var dataSet = emptyList<Record>()
+    private var dataSet = emptyList<Record>()
 
     class ViewHolder(private val binding: ValuesTableRowItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(record: Record) {
-            binding.timestamp.text = record.timestamp.toString()
+            binding.timestamp.text = DateTimeFormatter
+                .ofLocalizedDateTime(FormatStyle.MEDIUM)
+                .withZone(ZoneId.systemDefault())
+                .format(record.timestamp)
             binding.value.text = record.value.toString()
         }
     }
